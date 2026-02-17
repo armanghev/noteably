fa5cdbb2f9639fcae8456578877e46dc7b131dbd
SourceCodeArman <armanghevondyan2008@gmail.com>
2026-02-16 23:31:10 -0800

    fix(ios): expand landscape VStack to full width so GeometryReader measures container

--- Noteably/Noteably/Views/Notes/CornellNotesView.swift
+++ Noteably/Noteably/Views/Notes/CornellNotesView.swift
@@ -77,6 +77,7 @@ struct CornellNotesView: View {
                 )
             }
         }
+        .frame(maxWidth: .infinity, alignment: .leading)
         .background(
             GeometryReader { geo in
                 Color.clear
